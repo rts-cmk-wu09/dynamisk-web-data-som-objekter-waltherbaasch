f06814a101cd2c7ef9f4031314b16e0504ee6202
waltherbaasch <U2444127@rts-365.dk>
2023-02-07 11:24:46 +0100

    added opgave 2

--- dynamiskweb.js
+++ dynamiskweb.js
@@ -9,4 +9,17 @@ const person = {
   console.log(person['lastName']); // Udskriver "Madsen"
   console.log(person.age); // Udskriver 30
   console.log(person['job']); // Udskriver "Udvikler"
-  
+  //opgave 2
+  copiedPerson.isMarried = true;
+  copiedPerson.hobbies = ['læse', 'rejse', 'programmering'];
+  copiedPerson.address = {
+    street: '123 Main St',
+    city: 'San Francisco',
+    state: 'CA',
+    zip: '94105'
+  };
+  console.log('Address:');
+console.log(copiedPerson.address.street);
+console.log(copiedPerson.address.city);
+console.log(copiedPerson.address.state);
+console.log(copiedPerson.address.zip);
