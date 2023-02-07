4ce35debb19e3a463e5e8af2d5b0a454b11c6894
waltherbaasch <U2444127@rts-365.dk>
2023-02-07 11:03:37 +0100

    added js

--- dynamiskweb.js
+++ dynamiskweb.js
@@ -1,6 +1,12 @@
-let person = {
-    firstname: "Mike",
-    lastname: "Madsen",
-    married: true,
-    age: 69, 
-  }
+const person = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 30,
+    job: 'developer'
+  };
+  
+  console.log(person.firstName); // Udskriver "John"
+  console.log(person['lastName']); // Udskriver "Doe"
+  console.log(person.age); // Udskriver 30
+  console.log(person['job']); // Udskriver "developer"
+  
